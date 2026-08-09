2d09ba48b95a81306cfc9e1f4e544acfda5f4e6f
GhayasAwan <ghayasawan662@gmail.com>
2026-08-10 03:00:23 +0500

    fix: add xlink:href and preserveAspectRatio to hero-bg.svg and logo.svg so background renders in img tag

--- scripts/convertImagesToSvg.js
+++ scripts/convertImagesToSvg.js
@@ -2,13 +2,12 @@ import fs from "fs";
 import path from "path";
 
 function getPngDimensions(buffer) {
-  // PNG IHDR chunk is at byte 8; width is at 16..20, height at 20..24
   const width = buffer.readUInt32BE(16);
   const height = buffer.readUInt32BE(20);
   return { width, height };
 }
 
-function convertPngToSvg(pngPath, svgPath) {
+function convertPngToSvg(pngPath, svgPath, isBg = false) {
   if (!fs.existsSync(pngPath)) {
     console.error("File not found:", pngPath);
     return;
@@ -18,8 +17,10 @@ function convertPngToSvg(pngPath, svgPath) {
   const base64 = buffer.toString("base64");
   const dataUri = `data:image/png;base64,${base64}`;
 
-  const svgContent = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 ${width} ${height}" width="100%" height="100%">
-  <image width="${width}" height="${height}" href="${dataUri}"/>
+  const aspect = isBg ? 'preserveAspectRatio="xMidYMid slice"' : 'preserveAspectRatio="xMidYMid meet"';
+
+  const svgContent = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 ${width} ${height}" width="100%" height="100%" ${aspect}>
+  <image width="${width}" height="${height}" href="${dataUri}" xlink:href="${dataUri}"/>
 </svg>`;
 
   fs.writeFileSync(svgPath, svgContent, "utf8");
@@ -27,10 +28,9 @@ function convertPngToSvg(pngPath, svgPath) {
 }
 
 const assetsDir = path.join(process.cwd(), "src", "assets");
-convertPngToSvg(path.join(assetsDir, "logo.png"), path.join(assetsDir, "logo.svg"));
-convertPngToSvg(path.join(assetsDir, "hero-bg.png"), path.join(assetsDir, "hero-bg.svg"));
+convertPngToSvg(path.join(assetsDir, "logo.png"), path.join(assetsDir, "logo.svg"), false);
+convertPngToSvg(path.join(assetsDir, "hero-bg.png"), path.join(assetsDir, "hero-bg.svg"), true);
 
-// Also convert any other png/jpg in assets just in case
 const allFiles = fs.readdirSync(assetsDir);
 allFiles.forEach((file) => {
   if (file.endsWith(".jpg") || file.endsWith(".jpeg")) {
@@ -39,8 +39,8 @@ allFiles.forEach((file) => {
     const buffer = fs.readFileSync(filePath);
     const base64 = buffer.toString("base64");
     const dataUri = `data:image/jpeg;base64,${base64}`;
-    const svgContent = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 800" width="100%" height="100%">
-  <image width="1200" height="800" href="${dataUri}"/>
+    const svgContent = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1200 800" width="100%" height="100%" preserveAspectRatio="xMidYMid slice">
+  <image width="1200" height="800" href="${dataUri}" xlink:href="${dataUri}"/>
 </svg>`;
     fs.writeFileSync(svgPath, svgContent, "utf8");
     console.log(`Converted JPG ${file} -> SVG`);
